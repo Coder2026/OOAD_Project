0ef977bd2f957f887c842cc3652d7e7c6134cb6f
Katrina3112 <114214461+Katrina3112@users.noreply.github.com>
2024-12-18 23:56:40 +0700

    katrina update2

--- src/view/VendorViewAcceptedInvitation.java
+++ src/view/VendorViewAcceptedInvitation.java
@@ -12,17 +12,21 @@ import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 import model.Event;
+import model.Invitation;
 import model.User;
 import util.Response;
 
@@ -32,11 +36,18 @@ public class VendorViewAcceptedInvitation{
 	private Stage primaryStage;
 	private String id;
 	private VBox root;
+	private Label successLabel;
+    private Label errorLabel;
+    private TextField nameField;
+    private DatePicker datePicker;
+    private TextField locationField;
+    private TextField descField;
 
 	public void show(Stage primaryStage, String Id) {
 		// TODO Auto-generated method stub
 		this.primaryStage = primaryStage;
 		this.id = id;
+		
 		VBox root = createLayout(primaryStage);
 		Scene scene = new Scene (root, 1200, 800);
 		primaryStage.setTitle("View Accepted Events");
@@ -50,13 +61,15 @@ public class VendorViewAcceptedInvitation{
 		// TODO Auto-generated method stub
 		GridPane grid = createGridPane();
 		TableView<Event> eventTable = createTable();
-		Label eventLabel = new Label();
+		
+		Label eventLabel = new Label("View Accepted Events");
+		GridPane.setHalignment(eventLabel, HPos.CENTER);
+		
 		 Button homeBtn = new Button("Home");
 	        homeBtn.setOnAction(event ->{
 	        	new VendorHomeView().show(primaryStage);
 	        });
-		eventLabel.setText("View Accepted Events");
-		GridPane.setHalignment(eventLabel, HPos.CENTER);
+		
 		grid.add(eventLabel, 0, 0);
 		grid.add(eventTable, 0, 1);
 		grid.add(homeBtn, 0, 2);
@@ -73,19 +86,48 @@ public class VendorViewAcceptedInvitation{
 	     return grid;
 	}
 
-	
+	private TableColumn<Event, Void> createDetailColumn() {
+		successLabel = createErrorLabel(Color.GREEN);
+        errorLabel = createErrorLabel(Color.RED);
+        
+        TableColumn<Event, Void> buttonColumn = new TableColumn<>("Detail");
+        buttonColumn.setCellFactory(column -> new TableCell<Event, Void>() {
+            private final Button viewButton = new Button("View List");
+
+            {
+                viewButton.setOnAction(event -> {
+                    Event selectedEvent = getTableView().getItems().get(getIndex());
+                    if (selectedEvent != null) {
+                        new VendorEventDetailView().show(primaryStage, selectedEvent.getEvent_id(), selectedEvent);
+                    }
+                });
+            }
+
+            @Override
+            protected void updateItem(Void item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setGraphic(null);
+                } else {
+                    setGraphic(viewButton);
+                }
+            }
+        });
+        return buttonColumn;
+    }
 
 	private TableView<Event> createTable() {
 	        
-		 TableView<Event> tableView = new TableView<>();
-
+		 tableView = new TableView<>();
+		 tableView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
 	        tableView.getColumns().addAll(
 	            createColumn("Event ID", "event_id"),
 	            createColumn("Event Name", "event_name"),
 	            createColumn("Event Date", "event_date"),
 	            createColumn("Event Location", "event_location"),
 	            createColumn("Event Description", "event_description"),
-	            createColumn("Event Organizer", "event_organizer")
+	            createColumn("Event Organizer", "event_organizer"),
+	            createDetailColumn()
 	        );
 
 		    return tableView;
@@ -99,15 +141,20 @@ public class VendorViewAcceptedInvitation{
 	
 	public void loadData(String eventId) {
         VendorController controller = new VendorController();
-        //Response<List<User>> response = controller.getAllAcceptedEvent(eventId);
+        Response<List<Invitation>> response = controller.viewAcceptedInvitations(id);
 
-//        if (response.isSuccess()) {
-//            ObservableList<User> eventData = FXCollections.observableArrayList(response.getData());
-//            //tableView.setItems(eventData);
-//
-//        }
+        if (response.isSuccess()) {
+            //ObservableList<Invitation> eventData = FXCollections.observableArrayList(response.getData());
+            //tableView.setItems(eventData);
+
+        }
     }
 	
+	private Label createErrorLabel(Color color) {
+        Label errorLabel = new Label();
+        errorLabel.setTextFill(color);
+        return errorLabel;
+    }
 	
 	
 }
